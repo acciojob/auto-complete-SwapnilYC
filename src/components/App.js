3a683bd3d13f1a1453f64fc648f9bdc8cf3fb300
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-06-23 04:43:01 +0000

    Code added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+
 const App = () => {
+  const [search, setSearch] = useState("");
+  const [filteredItem, setFilteredItem] = useState([...fruits]);
+
+  useState(() => {
+    if (!search) setFilteredItem([...fruits]);
+    else setFilteredItem = filteredItem.filter((fruit) => fruit.includes(search));
+  }, [search])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" onInput={(e) => setSearch(e.target.value)}></input>
+      {/* Do not remove the main div */}
+      <ul>
+        {
+          filteredItem.map((filteredFruit) => (
+            <li>{filteredFruit}</li>
+          ))
+        }
+      </ul>
+
     </div>
   )
 }
 
-export default App
+// const App = () => {
+//   return(
+//     <div>
+//       <h1>Hello</h1>
+//     </div>
+//   )
+// }
+
+export default App;
